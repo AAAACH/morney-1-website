be6954213425fe2c30671f8529de3847f55bb958
CHEN <812607138@qq.com>
2023-04-30 03:21:39 +0800

    deploy

---
+++ precache-manifest.9154e1ab3f517b4058d842480343d0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e838a7af5cb8ffc3546c959e2117807e",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "d02b99a5b1d1d67b8c72",
+    "url": "/morney-1-website/static/css/main.36ff1861.chunk.css"
+  },
+  {
+    "revision": "424267f56e4b40b24ae0",
+    "url": "/morney-1-website/static/js/2.d93d9a14.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-1-website/static/js/2.d93d9a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02b99a5b1d1d67b8c72",
+    "url": "/morney-1-website/static/js/main.c29140b9.chunk.js"
+  },
+  {
+    "revision": "4c168aef22a9f8ea1039",
+    "url": "/morney-1-website/static/js/runtime-main.ae89cde8.js"
+  }
+]);
